f13a170e1ac44489dba5ed4e5209f33691634060
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-06-25 14:06:59 +0300

    Upgrade Atata.Cli package to v1.1.0; Add SetUpFixture to Atata.Cli.HtmlValidate.IntegrationTests with NpmCli.InstallIfMissing functionality

---
+++ test/Atata.Cli.HtmlValidate.IntegrationTests/SetUpFixture.cs
@@ -0,0 +1,16 @@
+﻿using Atata.Cli.Npm;
+using NUnit.Framework;
+
+namespace Atata.Cli.HtmlValidate.IntegrationTests
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            new NpmCli()
+                .InstallIfMissing(HtmlValidateCli.Name, global: true);
+        }
+    }
+}
