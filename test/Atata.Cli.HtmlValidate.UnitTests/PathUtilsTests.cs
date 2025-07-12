50341429956124d0fdb2d7e0ac874213598ed9b0
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-07-12 09:57:02 +0200

    Update `PathUtilsTests.GetRelativePath` test

--- test/Atata.Cli.HtmlValidate.UnitTests/PathUtilsTests.cs
+++ test/Atata.Cli.HtmlValidate.UnitTests/PathUtilsTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Atata.Cli.HtmlValidate.UnitTests;
 
@@ -12,12 +11,12 @@ public static class PathUtilsTests
         [TestCase(@"D:\a\b\c\", @"D:\a\b\c\file.txt", ExpectedResult = @"file.txt")]
         [TestCase(@"D:\a\b\c\", @"D:\a\b\c\d\file.txt", ExpectedResult = @"d\file.txt")]
         [TestCase(@"D:\a\b\c\", @"C:\file.txt", ExpectedResult = @"C:\file.txt")]
+        [Platform("Win")]
         public string WithValidValues(string relativeTo, string path)
         {
             string result = PathUtils.GetRelativePath(relativeTo, path);
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                Assert.That(result, Is.EqualTo(Path.GetRelativePath(relativeTo, path)));
+            Assert.That(result, Is.EqualTo(Path.GetRelativePath(relativeTo, path)));
 
             return result;
         }
